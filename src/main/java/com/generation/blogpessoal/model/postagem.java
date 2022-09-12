73fa3cf7004d8d9165133a43bb9e6c35a9d098e1
maikon santos <maikoonsantoz@gmail.com>
2022-09-12 18:55:32 -0300

    blogpessoal Security

--- src/main/java/com/generation/blogpessoal/model/postagem.java
+++ src/main/java/com/generation/blogpessoal/model/postagem.java
@@ -38,6 +38,10 @@ public class postagem {
 		@JsonIgnoreProperties("postagem")
 		private Tema tema;
 		
+		@ManyToOne
+		@JsonIgnoreProperties("postagem")
+		private Usuario usuario;
+		
 		
 		public Long getId() {
 			return id;
@@ -77,6 +81,14 @@ public class postagem {
 
 		public void setTema(Tema tema) {
 			this.tema = tema;
+		}
+
+		public Usuario getUsuario() {
+			return usuario;
+		}
+
+		public void setUsuario(Usuario usuario) {
+			this.usuario = usuario;
 		} 
 		
 		}
